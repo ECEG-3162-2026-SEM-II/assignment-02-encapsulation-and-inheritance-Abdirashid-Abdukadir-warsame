fa15a5d598361bb8cabce0438202aacf8e5fdc86
Abdirashid-Abdukadir-warsame <enwarkomad@gmail.com>
2026-03-30 21:48:57 +0300

    create-another-one-subclass

---
+++ src/CheckingAccount.java
@@ -0,0 +1,17 @@
+public class CheckingAccount extends BankAccount {
+    private static final double TRANSACTION_FEE = 1.50;
+
+    public CheckingAccount(String accountHolder, double initialBalance) {
+        super(accountHolder, initialBalance);
+    }
+
+    // Override withdraw
+    @Override
+    public void withdraw(double amount) {
+        double totalAmount = amount + TRANSACTION_FEE;
+
+        if (amount > 0 && balance >= totalAmount) {
+            super.withdraw(totalAmount); // reuse parent logic
+        }
+    }
+}
